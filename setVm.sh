f5bbcc0c7b1c9c14a9064288be9ef0debb7c3d64
Esther Lopez Ramos <eslora96@gmail.com>
2018-03-16 12:54:26 +0100

    properties changed

--- setVm.sh
+++ setVm.sh
@@ -1,3 +1,8 @@
+# Ddownloads and set apache-jmeter
+#wget http://ftp.cixug.es/apache//jmeter/binaries/apache-jmeter-4.0.tgz
+#tar -xzvf apache-jmeter-4.0.tgz
+
+# Set both vm 1 and 2 
 tar -zxf si2srv.tgz
 mv si2srv si2srv1
 
